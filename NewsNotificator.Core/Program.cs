f4f98ffefdb414ec57ef3aca291918a716b4419e
Tsubasa <26428614+kamome283@users.noreply.github.com>
2026-05-29 10:25:42 +0900

    バックグラウンドサービス `Worker` をローカルスコープ内に移動し、構造を簡素化

--- NewsNotificator.Core/Program.cs
+++ NewsNotificator.Core/Program.cs
@@ -1,7 +1,21 @@
-using NewsNotificator.Core;
-
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
 host.Run();
+
+file class Worker(ILogger<Worker> logger) : BackgroundService
+{
+  protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+  {
+    while (!stoppingToken.IsCancellationRequested)
+    {
+      if (logger.IsEnabled(LogLevel.Information))
+      {
+        logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+      }
+
+      await Task.Delay(1000, stoppingToken);
+    }
+  }
+}
