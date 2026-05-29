ba0b7941ae465fa24e540f6a2260154c4f497394
Tsubasa <26428614+kamome283@users.noreply.github.com>
2026-05-29 10:24:11 +0900

    Core プロジェクトを AppHost に統合し、依存関係を追加

--- NewsNotificator.AppHost/AppHost.cs
+++ NewsNotificator.AppHost/AppHost.cs
@@ -1,7 +1,15 @@
 // ReSharper disable UnusedVariable
+
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var db = builder
   .AddSqlite("db");
+
+var core = builder
+  .AddProject<NewsNotificator_Core>("core")
+  .WithReference(db)
+  .WaitFor(db);
 
 builder.Build().Run();
